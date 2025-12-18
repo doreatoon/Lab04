3015551e05ad87dd0d3d9fbb7cbc80f8b186d413
doreatoon <chichengan50@gmail.com>
2025-12-18 15:13:31 +0700

    Lab04

---
+++ src/models/student.ts
@@ -0,0 +1,6 @@
+//problem 2
+export type Student = {
+id: number;
+name: string;
+major: string;
+};
---
+++ src/test-students.ts
@@ -0,0 +1,6 @@
+//problem 2
+import { loadStudents, saveStudents, addStudent } from "./services/studentService";
+const students = loadStudents();
+const updated = addStudent(students, "Mina", "UX");
+saveStudents(updated);
+console.log("Updated students:", updated);
